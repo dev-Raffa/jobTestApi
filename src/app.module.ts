8e0e7ec665ebb58cb8f993362d4d0e8dba6fb24d
dev-Raffa <mnraffaell@gmail.com>
2023-12-24 03:20:40 -0200

    ref(module): refact app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClassModule } from './class/class.module';
+import { CourseModule } from './course/course.module';
+import { ProfessorModule } from './professor/professor.module';
+import { UserModule } from './user/user.module';
+import { UserAdminModule } from './user-admin/user-admin.module';
+import { ClassEntity } from './class/model/class.entity';
+import { CourseEntity } from './course/model/course.entity';
+import { ProfessorEntity } from './professor/model/professor.entity';
+import { userEntity } from './user/model/user.entity';
+import { userAdminEntity } from './user-admin/model/user-admin.entity';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [ ClassEntity, CourseEntity, ProfessorEntity, userEntity, userAdminEntity],
+    synchronize: true
+  }),
+    ClassModule,
+    CourseModule,
+    ProfessorModule,
+    UserModule,
+    UserAdminModule,
+  ],
 })
 export class AppModule {}
