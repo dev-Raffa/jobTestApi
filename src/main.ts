36b16ebf7fc0124557a7159b309862d62c19230d
dev-Raffa <mnraffaell@gmail.com>
2024-01-03 20:25:44 -0200

    upgrade

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: '*',
+      origin: 'https://job-test-inhsag2h3-rafaelconceicao.vercel.app/',
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true,
       optionsSuccessStatus: 204,
