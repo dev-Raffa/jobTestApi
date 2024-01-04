af35cd4f5357016fe327f5257df2ef4fc505cf49
Rafael da Conceição <mnraffaell@gmail.com>
2024-01-04 13:02:58 -0300

    cors localhost

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
         'https://job-test-app.vercel.app',
         'https://job-test-app-rafaelconceicao.vercel.app',
         'https://job-test-app-git-main-rafaelconceicao.vercel.app',
+        'http://localhost:5173',
       ],
       allowedHeaders: ['Content-Type', 'Accept'],
       methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
