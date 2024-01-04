cf4bb0864a108df56507227ae24fe3c58df5f01d
Rafael da Conceição <mnraffaell@gmail.com>
2024-01-04 10:49:26 -0300

    config cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [
+        'https://job-test-app.vercel.app',
+        'https://job-test-app-rafaelconceicao.vercel.app',
+        'https://job-test-app-git-main-rafaelconceicao.vercel.app',
+      ],
+      allowedHeaders: ['Content-Type', 'Accept'],
+      methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+      credentials: true,
+      optionsSuccessStatus: 204,
+      preflightContinue: false,
+    },
+  });
   await app.listen(3000);
 }
 
